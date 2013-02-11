ade625da9da5f77731c15f8206767cbcee4086c2
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-02-11 12:29:32 +0100

    first example of client lib

---
+++ user.py
@@ -0,0 +1,134 @@
+
+
+class Restriction(object):
+    def __init__(self, attrs):
+        for key, value in attrs.iteritems():
+            setattr(self, key, value)
+
+    def allows(self, attr, *args, **kwargs):
+        return False
+
+    def did(self, attr, *args, **kwargs):
+        pass
+
+
+class BinaryRestriction(Restriction):
+
+    def allows(self, attr, *args, **kwargs):
+        return self.allow
+
+
+class TotalAmountRestriction(Restriction):
+
+    def allows(self, attr, *args, **kwargs):
+        if not self.left:
+            return False
+
+        change = args and args[0] or 1
+        return self.left - change >= 0
+
+    def did(self, attr, *args, **kwargs):
+        change = args and args[0] or 1
+        self.left -= change
+
+
+class PerTimeRestriction(TotalAmountRestriction):
+    pass
+
+
+class AccountAmountRestriction(Restriction):
+    # FIME: needs to be implemented
+    pass
+
+class JerryUser(object):
+
+    PROVIDER_CLASS = None
+    RESTRICTIONS = {
+        "BinaryRestriction": BinaryRestriction,
+        "PerTimeRestriction": PerTimeRestriction,
+        "TotalAmountRestriction": TotalAmountRestriction,
+        "AccountAmountRestriction": AccountAmountRestriction,
+    }
+
+    def __init__(self, profile_state=None):
+        if profile_state:
+            self._load_state(profile_state)
+
+    def _load_state(self, profile_state):
+        self.profile_name = profile_state.get("profile", None)
+        self.default = profile_state['default'] == 'allow'
+        self.restrictions = self._compile_restrictions(profile_state["states"])
+
+    def _compile_restrictions(self, states):
+        return dict([(key, [self.RESTRICTIONS[item['class_']](item) \
+                                for item in values])
+                        for key, values in states.iteritems()])
+
+    def can(self, action, *args, **kwargs):
+        restrictions = self.restrictions.get(action, "")
+        if not restrictions:
+            return self.default
+
+        for restriction in restrictions:
+            if not restriction.allows(action, *args, **kwargs):
+                return False
+
+        return not self.default
+
+    def did(self, action, *args, **kwargs):
+        restrictions = self.restrictions.get(action, "")
+        if restrictions:
+            for restriction in restrictions:
+                restriction.did(action, *args, **kwargs)
+
+    def has(self, attr):
+        return self.default
+
+
+if __name__ == "__main__":
+    import sure
+
+    profile_state = {
+        'profile': 'free',
+        'default': 'deny',
+        'states': {
+            'take_photo': [
+                {'class_': 'BinaryRestriction', 'allow': True}
+            ],
+            'upload_photo': [
+                {'class_': 'PerTimeRestriction', 'limit_to': 100,
+                        'duration': 24 * 60 * 60, 'left': 3}
+            ],
+            'share_photo': [
+                {'class_': 'PerTimeRestriction', 'limit_to': 100,
+                        'duration': 24 * 60 * 60, 'left': 20},
+                {'class_': 'TotalAmountRestriction', 'total_max': 100,
+                        'left': 10}
+            ],
+        }
+    }
+
+    user = JerryUser(profile_state=profile_state)
+
+    user.can("take_photo").should.be.ok
+    user.can("take_photo", 10).should.be.ok
+
+    user.can("upload_photo").should.be.ok
+    user.can("upload_photo", 3).should.be.ok
+    user.can("upload_photo", 4).should.be.false
+
+    user.can("share_photo").should.be.ok
+    user.can("share_photo", 9).should.be.ok
+    # total amount kicks in though daily woud allow it
+    user.can("share_photo", 12).should.be.false
+
+    # not defined: default is deny
+    user.can("view_photo").should.be.false
+    user.can("view_photo", 10).should.be.false
+
+    ## lets modify and play again:
+    user.did("upload_photo")
+    user.can("upload_photo").should.be.ok
+    user.can("upload_photo", 2).should.be.ok
+    user.did("upload_photo", 2)
+    user.can("upload_photo").should.be.false
